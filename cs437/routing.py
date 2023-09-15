7c25b7c93672991d24658648f35d043d7ff023d2
Anthony Doan <=>
2023-09-15 15:20:06 -0500

    Update car starting point

--- cs437/routing.py
+++ cs437/routing.py
@@ -120,7 +120,7 @@ def add_buffer(grid):
 def run():
     threshold = 100  # Set threshold (can adjust as needed)
     start = (picar_position['x'], picar_position['y'])
-    goal = (100, 100)
+    goal = (0, 50)
 
     while start != goal:
         scan_map = update_map(threshold)
