4e55eacf93f91320705ce71e148c0d2f81ae7d05
Anthony Doan <=>
2023-09-15 15:41:40 -0500

    Update start

--- cs437/routing.py
+++ cs437/routing.py
@@ -118,7 +118,7 @@ def add_buffer(grid):
 # SLAM with ultrasonic sensor
 def run():
     threshold = 100  # Set threshold (can adjust as needed)
-    start = (0,0)
+    start = (0,50)
     goal = (200, 200)
 
     while start != goal:
