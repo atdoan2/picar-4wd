ffbf248d4d4ec7c8f4901f1388d6000c76a64b29
Anthony Doan <=>
2023-09-15 15:06:42 -0500

    reduce buffer

--- cs437/routing.py
+++ cs437/routing.py
@@ -133,7 +133,7 @@ def run():
 
     while start != goal:
         updated_map = update_map(threshold)
-        buffered_map = add_buffer(add_buffer(add_buffer(updated_map)))
+        buffered_map = add_buffer(updated_map)
         
         for row in buffered_map:
             for elem in row:
