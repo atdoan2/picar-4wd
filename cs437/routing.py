7309d740f79b925fa51d02b539f1e33d4cc6c9e5
Anthony Doan <=>
2023-09-15 18:17:47 -0500

    Print map out

--- cs437/routing.py
+++ cs437/routing.py
@@ -124,10 +124,10 @@ def run():
         scan_map = update_map(threshold)
         buffered_map = add_buffer(add_buffer(add_buffer(scan_map)))
         
-        # for row in buffered_map:
-        #     for elem in row:
-        #         print(elem,end="")
-        #     print()
+        for row in buffered_map:
+            for elem in row:
+                print(elem,end="")
+            print()
         
         moves = []
         
