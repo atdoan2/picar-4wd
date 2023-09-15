581f0212fb5fa1257a5e5fb6f3b2330e320f661b
Anthony Doan <=>
2023-09-15 17:34:24 -0500

    Update movement directions for x,y

--- cs437/routing.py
+++ cs437/routing.py
@@ -43,7 +43,7 @@ def update_map(threshold):
 
     return picar_map
 
-movements = [(1, 0, "up"), (-1, 0, "down"), (0, 1, "right"), (0, -1, "left")]
+movements = [(0,1, "up"), (0,-1, "down"), (1, 0, "right"), (-1, 0, "left")]
 
 def heuristic(current, goal):
     # Calculate the Manhattan distance as the heuristic
