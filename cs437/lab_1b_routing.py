68434850936718ae42ab771d6f980e44c2203dab
Divya Manirajan <divyamanirajan@Divyas-MacBook-Pro-2.local>
2023-09-16 16:44:41 -0500

    update lab_1b_routing

--- cs437/lab_1b_routing.py
+++ cs437/lab_1b_routing.py
@@ -261,6 +261,7 @@ def run():
                         start = (start[0] + 1, start[1])
                 print("start: ", start)
                 print("goal: ", goal)
+                time.sleep(1)
             else:
                 print("No path found")
             
