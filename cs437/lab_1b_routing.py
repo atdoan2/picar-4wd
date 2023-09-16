cd9144ed927f7ea6eab822a8c7798e231176b632
Divya Manirajan <divyamanirajan@Divyas-MacBook-Pro-2.local>
2023-09-16 15:24:55 -0500

    update lab_1b_routing

--- cs437/lab_1b_routing.py
+++ cs437/lab_1b_routing.py
@@ -229,7 +229,7 @@ def run():
                         time.sleep(1)
                         fc.stop()
                         fc.turn_left(100)
-                        time.sleep(2)
+                        time.sleep(2.5)
                         fc.forward(1)
                         time.sleep(1)
                         fc.turn_right(100)
