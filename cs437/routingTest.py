1aa32d25aa6589c3e28ebb1fbe3f88482680078f
Divya Manirajan <divyamanirajan@Divyas-MacBook-Pro-2.local>
2023-09-16 01:09:19 -0500

    update routingTest

--- cs437/routingTest.py
+++ cs437/routingTest.py
@@ -189,7 +189,7 @@ def run():
         goal = (0,map_width/2)
         
         
-        if current_angle == 180:
+        if current_angle == 170:
 
             moves = []
             path, move_directions = astar_search(buffered_map, start, goal)
