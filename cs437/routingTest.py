dee3d8ea313d86e6dccc75421e912814f2e2e2db
Divya Manirajan <divyamanirajan@Divyas-MacBook-Pro-2.local>
2023-09-16 01:13:47 -0500

    update routingTest

--- cs437/routingTest.py
+++ cs437/routingTest.py
@@ -174,7 +174,7 @@ def add_buffer(grid):
 
 # SLAM with ultrasonic sensor
 def run():
-    threshold = 100  # Set threshold (can adjust as needed)
+    threshold = 20  # Set threshold (can adjust as needed)
     while True:
         updated_map = update_map(picar_position, threshold)
         
