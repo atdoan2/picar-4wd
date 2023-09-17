5273973cf9590171077dfd8068bb4046d4ec9c4a
Divya Manirajan <divyamanirajan@Divyas-MacBook-Pro-2.local>
2023-09-17 10:51:37 -0500

    update self_driving for video

--- cs437/lab_1b_self_driving.py
+++ cs437/lab_1b_self_driving.py
@@ -241,7 +241,7 @@ def run():
                         time.sleep(1)
                         fc.stop()
                         fc.turn_left(100)
-                        time.sleep(3)
+                        time.sleep(2)
                         fc.forward(1)
                         time.sleep(.1)
                         fc.turn_right(100)
